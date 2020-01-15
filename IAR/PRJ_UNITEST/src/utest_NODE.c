c5b6535d64c32aa5f1884563c672e0e42493d1fb
Oleg Makshanov <pvp@dcconsult.ru>
2020-01-15 22:07:48 +0300

    Код
    В функции suite_NODE добавил переход в режим PM2. Электрически не проверял, но судя по отладочному выводу все работает.

--- IAR/PRJ_UNITEST/src/utest_NODE.c
+++ IAR/PRJ_UNITEST/src/utest_NODE.c
@@ -1,11 +1,13 @@
 #include "utest_suite.h"
 #include "manager.h"
 #include "nwdebuger.h"
+#include "ioCC2530.h"
 
 // Собирать проект без ключем GATEWAY в файле basic.h
 
 void suite_NODE(void)
 {
+  SLEEPCMD |= 0x06; // Select PM2
    bool con = MG_Connect();
   if (con)
   {
@@ -16,5 +18,9 @@ void suite_NODE(void)
     LOG_ON("Network connect faild");
   }
   
-  while(1);
+  while(1)
+  {
+    LOG_ON("Go PM2");
+    PCON = 1;
+  };
 }
